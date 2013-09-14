5a28d6e6320f7314e4b772ed2c9bf01067a6a427
Idris <sld7700@gmail.com>
2013-09-15 03:56:01 +0400

    Updated .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityDictApi::Application.config.secret_token = '4df6989e057522c2121af332c30c36aa5a3b0ca1574ec6f2c02e4aecc4e2c61ba7b98459d2d4f7d459217340fa4a7ab4e0379fb225d128de87c8a216f088a878'
