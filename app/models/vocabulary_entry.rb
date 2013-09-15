d275a1e2527f66d4a4bffc70eb3ea10d61b3d962
Idris <sld7700@gmail.com>
2013-09-15 08:27:46 +0400

    Added validation to name in VocabularyEntry. Updated TomitaFactsExtractor

--- app/models/vocabulary_entry.rb
+++ app/models/vocabulary_entry.rb
@@ -5,6 +5,8 @@ class VocabularyEntry < ActiveRecord::Base
 
   belongs_to :city
 
+  validate :city_and_name_uniqueness
+
 
   def own_type
     #[nil, "fuel", "library", "post_office", "hospital", "bus_station", "bank",
@@ -16,5 +18,13 @@ class VocabularyEntry < ActiveRecord::Base
     # "doctors", "arts_centre", "dentist", "bar", "fountain", "pub", "nightclub", "atm",
     # "fire_station", "swimming_pool", "grave_yard", "bench", "driving_school", "register_office", "courthouse",
     # "recycling", "taxi", "townhall", "social_facility", "prison", "sanatorium", "bicycle_rental", "community_centre"]
+  end
+
+
+  private
+
+
+  def city_and_name_uniqueness
+    errors.add(:base, "City id and name is not uniqueness") if VocabularyEntry.find_by_city_id_and_name( city_id, name )
   end
 end
--- lib/tomita_facts_extractor.rb
+++ lib/tomita_facts_extractor.rb
@@ -26,11 +26,48 @@ class TomitaFactsExtractor
       hash[new_root] = v
     end
 
-    return hash
+    last_hash = {}
+    hash.each do |k,v|
+      new_root = v["CITY"].first
+      v.delete("CITY")
+      if v.present? && new_root
+        last_hash[new_root] ||= Set.new
+        last_hash[new_root] <<  v
+      end
+    end
+
+    return last_hash
   end
+
 
   def self.lazy filename = "facts.xml"
     tomita = TomitaFactsExtractor.new filename
     puts tomita.get_facts
   end
+
+
+  def prepare_data_for_tomita
+    file = File.open("#{Rails.root}/project_files/tomita/for_tomita.txt", 'w')
+    for i in 1...470 do
+      fetch_remote_json( generate_url_for_rbcitynews( i ) ).each do |feed|
+        if feed["summary"]
+    	    file.puts CGI::unescapeHTML(feed["summary"]).gsub('.', " (feed#{feed["id"]}).")
+        end
+      end
+    end
+    file.close
+  end
+
+
+  def generate_url_for_rbcitynews page_num
+    "http://rbcitynews.ru/all.json?page=#{page_num}"
+  end
+
+
+  def fetch_remote_json(url)
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = resp.body
+    JSON.parse(data)
+  end
+
 end
