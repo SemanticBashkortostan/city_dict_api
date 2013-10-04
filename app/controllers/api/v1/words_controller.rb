985c690a6e33d68fa64ac8eb42f37a2ec1642034
Idris <sld7700@gmail.com>
2013-10-04 22:54:55 +0400

    Fixed error by :type_name filtering in words.json

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,20 +1,23 @@
 class Api::V1::WordsController < Api::V1::BaseController
 
   def index    
+    params[:page] ||= 1
+    params[:per_page] ||= VocabularyEntry::PAGINATES_COUNT
+
     scope = VocabularyEntry 
     if params[:city_id].present?      
       scope = scope.includes(:metadata).where( :metadata => { :city_id => params[:city_id] } )
     end
     if params[:type].present?
-      scope = scope.where(:metadata => {:type_name => params[:type]})     
+      scope = scope.includes(:metadata).where( :metadata => { :type_name => params[:type] } )     
     end
     if params[:name].present?
       scope = scope.where(:name => [params[:name], params[:name].mb_chars.capitalize.to_s])    
     end
 
     @words = scope.page(params[:page])
-    @pages_data = { :pages_count => @words.total_pages, :per_page => VocabularyEntry::PAGINATES_COUNT, 
-                    :current_page => params[:page] || 1 }
+    @pages_data = { :pages_count => @words.total_pages, :per_page => params[:per_page], 
+                    :current_page => params[:page] }
   end
 
 
