7b826c7a7f888835dff85257deeba2495c502090
sld <sld7700@gmail.com>
2014-08-12 18:17:53 +0600

    Deleted unnecessary tests and created test for VocabularyEntry#fuzzy_match.

--- test/unit/vocabulary_entry_test.rb
+++ test/unit/vocabulary_entry_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class VocabularyEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#fuzzy_match should find word' do
+    words = ['threq']
+    similarity_data = VocabularyEntry.fuzzy_match(words, 0.5, 0.5)
+    assert_equal(similarity_data[:word], 'threq')
+  end
+
+  test '#fuzzy_match should not find word' do
+    words = ['threq']
+    similarity_data = VocabularyEntry.fuzzy_match(words, 1.0, 1.0)
+    assert_equal(similarity_data, nil)
+  end
 end
