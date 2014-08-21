3a2d20333b6dfd1d146efa5922540a46a572407d
sld <sld7700@gmail.com>
2014-08-21 20:57:10 +0400

    Some notes in osm_fetcher.rb and update documentation.

--- lib/osm_fetcher.rb
+++ lib/osm_fetcher.rb
@@ -1,13 +1,19 @@
 require 'nokogiri'
 
+#TODO: 1) Refactor this class
+#      2) Add fetching lat and lon from .osm file
 class OsmFetcher
 
-
-  ISHIMBAY_BOUNDING_BOX = {:top => 53.4731, :left => 55.9502, :bottom => 53.3999, :right => 56.1322}
-  SALAVAT_BOUNDING_BOX = {:top => 53.3862, :left => 55.8489, :bottom => 53.3054, :right => 55.999}
-  STERLITAMAK_BOUNDING_BOX = {:top => 53.7523, :left => 55.7659, :bottom => 53.5619, :right => 56.1744}
-  NEFTEKAMSK_BOUNDING_BOX = {:top => 56.1428, :left => 54.1406, :bottom => 55.9649, :right => 54.4221}
-  UFA_BOUNDING_BOX = {:top => 54.8943, :left => 55.7638, :bottom => 54.5258, :right => 56.21}
+  ISHIMBAY_BOUNDING_BOX = {:top => 53.4731, :left => 55.9502,
+    :bottom => 53.3999, :right => 56.1322}
+  SALAVAT_BOUNDING_BOX = {:top => 53.3862, :left => 55.8489,
+    :bottom => 53.3054, :right => 55.999}
+  STERLITAMAK_BOUNDING_BOX = {:top => 53.7523, :left => 55.7659,
+    :bottom => 53.5619, :right => 56.1744}
+  NEFTEKAMSK_BOUNDING_BOX = {:top => 56.1428, :left => 54.1406,
+    :bottom => 55.9649, :right => 54.4221}
+  UFA_BOUNDING_BOX = {:top => 54.8943, :left => 55.7638,
+    :bottom => 54.5258, :right => 56.21}
 
 
   # +bounding_box+ is a Hash, look at up
@@ -17,21 +23,21 @@ class OsmFetcher
     @filename = path + filename
   end
 
-
-  def get_part_of_map( main_map = "#{Rails.root}/project_files/osm_maps/bashkortostan.osm" )
+  def get_part_of_map(main_map = "#{Rails.root}/project_files/osm_maps/bashkortostan.osm")
     raise "OsmFetcher - bashkortostan.osm not exist!" unless File.exist?(main_map)
-    exec = "osmosis --read-xml file=\"#{main_map}\" --bounding-box top=#{@bounding_box[:top]} left=#{@bounding_box[:left]} \
-            bottom=#{@bounding_box[:bottom]} right=#{@bounding_box[:right]} --write-xml file=\"#{@filename}\""
+    exec = "osmosis --read-xml file=\"#{main_map}\" --bounding-box \
+      top=#{@bounding_box[:top]} left=#{@bounding_box[:left]} \
+      bottom=#{@bounding_box[:bottom]} right=#{@bounding_box[:right]} \
+      --write-xml file=\"#{@filename}\""
     system exec
   end
-
 
   # Return array with elements like [name, amenity, osm_id]
   # where name.length > 2
   def get_features
     xmlfeed = Nokogiri::XML(open(@filename))
-    rows = xmlfeed.xpath("//*[@k=\"name\"]")
     features = []
+    #TODO: What is this? Why 0..1400?
     rows[0..1400].each do |row|
       name = row.attributes['v'].value
       osm_id = row.parent.attributes["id"].value
@@ -41,7 +47,6 @@ class OsmFetcher
     end
     return features
   end
-
 
   def self.make_maps_from_text_classes
     osm_arr = [
@@ -57,8 +62,7 @@ class OsmFetcher
     end
   end
 
-
-  #TODO: Add version of .osm file!!! But why?
+  #TODO: Handle updating of the .osm file!!!
   def self.fill_db!
     osm_arr = [
                       ["Sterlitamak", "sterlitamak.osm", OsmFetcher::STERLITAMAK_BOUNDING_BOX],
@@ -68,26 +72,31 @@ class OsmFetcher
                       ["Ufa", "ufa.osm", OsmFetcher::UFA_BOUNDING_BOX]
                ]
 
-    normalized_city_names_set = City.pluck(:name).map{|name| VocabularyEntry.get_normalized_name(name) }.to_set
+    normalized_city_names = City.pluck(:name).map do |name|
+      VocabularyEntry.get_normalized_name(name)
+    end
+    normalized_city_names_set = normalized_city_names.to_set
+
     osm_arr.each do |(city_eng_name, filename, bounding_box)|
       osm = OsmFetcher.new bounding_box, filename
       osm.get_features.each do |name, amenity, osm_id|
-        next if normalized_city_names_set.include?( VocabularyEntry.get_normalized_name( name ) )
-        token = VocabularyEntry.find_or_create_by_name_or_normalized_name name
+        normalized_name = VocabularyEntry.get_normalized_name(name)
+        next if normalized_city_names_set.include?(normalized_name)
+
+        token = VocabularyEntry.find_or_create_by_name_or_normalized_name(name)
 
         city_id = City.find_by_eng_name(city_eng_name).id
         type = amenity
-        metadata =  Metadata.find_or_create_by_city_id_and_source_and_type_name_and_vocabulary_entry_id(
-                city_id, :osm, type, token.id )
+        params = {city_id: city_id, source: :osm, type_name: type, vocabulary_entry_id: token.id}
+        metadata = Metadata.where(params).first || Metadata.create(params)
 
         unless metadata.other["osm_id"] || metadata.other["bounding_box"]
           metadata.other["osm_id"] = osm_id
           metadata.other["bounding_box"] = bounding_box.to_s
-          metadata.save!                
-        end                        
+          metadata.save!
+        end
       end
     end
   end
-
 
 end
